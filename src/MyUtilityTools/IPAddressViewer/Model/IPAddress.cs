bdf68d751ab99892f14ea77585466aabe86c0092
masjinno <masjinno@gmail.com>
2017-10-16 20:51:44 +0900

    #3 IPアドレス取得のためのソースコードファイルを追加

---
+++ src/MyUtilityTools/IPAddressViewer/Model/IPAddress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IPAddressShower
+{
+    class MyIPAddress
+    {
+        private string address;
+
+        public MyIPAddress()
+        {
+            SetAddress();
+        }
+
+        private void SetAddress()
+        {
+            string hostname = Dns.GetHostName();
+
+            // ホスト名からIPアドレスを取得する
+            IPAddress[] adrList = Dns.GetHostAddresses(hostname);
+
+            address = "";
+            foreach (IPAddress add in adrList)
+            {
+                if (add.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    address = add.ToString();
+                    //System.Windows.Forms.MessageBox.Show(address + "\n" + add.ToString(), "address");
+                }
+            }
+        }
+
+        public string GetAddress()
+        {
+            SetAddress();
+            return address;
+        }
+    }
+}
